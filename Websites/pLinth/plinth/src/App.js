21a7b9802255579c3f1b1dfaaae2e11d29737083
codechini <arangathvenuchinmay@gmail.com>
2022-05-21 11:35:27 +0530

    reg

--- Websites/pLinth/plinth/src/App.js
+++ Websites/pLinth/plinth/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import {BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 
 function App() {
     return ( 
-      <BrowserRouter>
-      <Navbar />
-        <Switch>
-          <Route exact path='/' component={Home} />
-        </Switch>
+      <BrowserRouter >
+        <Navbar />
+          <Switch >
+            <Route exact path = '/' component = { Home }/> 
+        </Switch> 
       </BrowserRouter>
     );
 }
