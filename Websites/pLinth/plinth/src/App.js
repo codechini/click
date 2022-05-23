e96a10db52b90d3abb51cf4fe56b9796808a6384
codechini <arangathvenuchinmay@gmail.com>
2022-05-23 08:29:25 +0530

    nothing done

--- Websites/pLinth/plinth/src/App.js
+++ Websites/pLinth/plinth/src/App.js
@@ -9,18 +9,26 @@ import AuthProvider from "./context/auth";
 import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-    return ( 
-      <AuthProvider>
-      <BrowserRouter>
+    return (
+
+        <AuthProvider >
+        <BrowserRouter >
         <Navbar />
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-          <PrivateRoute exact path="/profile" component={Profile} />
-          <PrivateRoute exact path="/" component={Home} />
-        </Switch>
-      </BrowserRouter>
-    </AuthProvider>
+        <Switch >
+        <Route exact path = "/register"
+        component = { Register }
+        /> 
+        <Route exact path = "/login" component = { Login }
+        /> 
+        <PrivateRoute exact path = "/profile"
+        component = { Profile }
+        /> 
+        <PrivateRoute exact path = "/"
+        component = { Home }
+        /> 
+        </Switch> 
+        </BrowserRouter> 
+        </AuthProvider>
     );
 }
 
