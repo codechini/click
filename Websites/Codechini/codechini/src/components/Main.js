845a3c598994962bde3a9dde4fd407265d143f78
codechini <arangathvenuchinmay@gmail.com>
2022-06-05 14:51:52 +0530

    tt

--- Websites/Codechini/codechini/src/components/Main.js
+++ Websites/Codechini/codechini/src/components/Main.js
@@ -35,7 +35,7 @@ const Main = () => {
             </Col>
           </Row>
           <div className="p-3">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/s4BibernJxU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="560" height="315" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
         </Container>
       </main>
