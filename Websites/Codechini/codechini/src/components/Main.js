919d5736b714aad810337daf5ad5d5d34a329d5b
codechini <arangathvenuchinmay@gmail.com>
2022-06-13 10:58:30 +0530

    Cc work in progress

--- Websites/Codechini/codechini/src/components/Main.js
+++ Websites/Codechini/codechini/src/components/Main.js
@@ -9,11 +9,15 @@ import {
   Row
 } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FaReact } from 'react-icons/fa';
+import { CgFigma } from 'react-icons/cg';
+import { IoLogoJavascript } from 'react-icons/io'
 
 
 const Main = () => {
   return (
       <main>
+        <div className="P1">
         <Container fluid>
           <Row className="fadeImg align-items-center p-0 mx-0">
             <Col xs={9} style={{ 
@@ -26,17 +30,31 @@ const Main = () => {
               <p style={{
                 fontSize:'28px',
                 color:'rgb(255,255,255)',
-                textAlign:'center',
+                textAlign:'end',
                 padding:'0px 0px 0px 0px',
                 }}>
-                Hi i'm Chinmay.A<br />I'm an Aspiring Front-end Web Developer <br />Skilled with JS , React.js websites & UX designing in Figma
+                Hi i'm Chinmay.A<br />
+                I’m a Front-end Web Developer with a year of UI & UX experience<br />
+                Currently i’m doing my 3<sup>rd</sup> year of my bachleor degree at Bhavan’s College.
               </p>
             </Col>
           </Row>
-          {/* <div className="p-3">
-          <iframe width="560" height="315" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div> */}
         </Container>
+        </div>
+        <div className="P2 d-flex align-items-center justify-content-center">
+          <Container fluid>
+            <p style={{
+              fontSize:'42px',
+              color:'rgb(243,95,27)',
+              // textAlign:'center',
+              padding:'0px 80px 0px 80px',
+              fontFamily: 'Rubik Moonrocks, cursive',
+              height:'150px'
+              }}>
+              Skilled with <IoLogoJavascript color="ead41c"/> , React.js <FaReact color="66d9ef"/> websites & UX designing in Figma<CgFigma color="a951ff"/>
+            </p>
+          </Container>
+        </div>
       </main>
   )
 }
