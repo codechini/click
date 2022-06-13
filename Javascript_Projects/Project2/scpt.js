bde40622d2e6c5606cc87ea0e93cfcf484a0f420
codechini <arangathvenuchinmay@gmail.com>
2022-06-13 10:57:48 +0530

    P2 js

--- Javascript_Projects/Project2/scpt.js
+++ Javascript_Projects/Project2/scpt.js
@@ -1,15 +1,15 @@
-//Just spaces
-//Just spaces
-//Just spaces
-// window.addEventListener('keydown', function(e) {
-//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-//     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-//     if (!audio) return;
-//     audio.currentTime = 0; // Sin esto no se reproduciria hasta terminar de reproducirse por primera vez
-//     audio.play();
-//     key.classList.add("playing");
-//     setTimeout(function() { key.classList.remove("playing"); }, 100);
-// });
+//spaces
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0; // Without this it would not play until it finished playing for the first time
+    audio.play();
+    key.classList.add("playing");
+    setTimeout(function() {
+        key.classList.remove("playing");
+    }, 100);
+});
 
 window.onload = function() {
     let iterable = [65, 83, 68, 70, 71, 72, 74, 75, 76];
@@ -17,10 +17,12 @@ window.onload = function() {
         document.querySelector(`.key[data-key="${value}"]`).addEventListener('click', function() {
             const audio = document.querySelector(`audio[data-key="${value}"]`);
             if (!audio) return;
-            // audio.currentTime = 0;
+            audio.currentTime = 0;
             audio.play();
             document.querySelector(`.key[data-key="${value}"]`).classList.add("playing");
-            setTimeout(function() { document.querySelector(`.key[data-key="${value}"]`).classList.remove("playing"); }, 100);
+            setTimeout(function() {
+                document.querySelector(`.key[data-key="${value}"]`).classList.remove("playing");
+            }, 100);
         });
     }
 }
