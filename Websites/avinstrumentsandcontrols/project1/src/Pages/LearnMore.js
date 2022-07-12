3a3630bdbd4430e01a242cd894d8ff932b188178
codechini <arangathvenuchinmay@gmail.com>
2022-07-12 18:04:53 +0530

    tt

---
+++ Websites/avinstrumentsandcontrols/project1/src/Pages/LearnMore.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LearnMore = () => {
+  return (
+  <>
+    <div className="aboutus d-flex justify-content-center align-items-center">
+      This is where you get quantity at it's finest.
+    </div>
+  </>
+  )
+}
+
+export default LearnMore;
