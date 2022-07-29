09a9b11ec8b9dee53839b013307d97189174a269
codechini <arangathvenuchinmay@gmail.com>
2022-07-29 09:28:09 +0530

    tt

---
+++ Websites/avinstrumentsandcontrols/project2/src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {
+  Card,
+  Col,
+  Row,
+  Button
+} from 'react-bootstrap';
+
+
+const Footer = (props) => {
+  return (
+    <>
+    <Card bg='dark' className="text-center text-white">
+      <Card.Header>{props.l1}</Card.Header>
+      <Card.Body bg='dark'>
+        <Card.Title>For further Information</Card.Title>
+        <Card.Text>
+          123-4567890
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+      <Card.Footer className="text-muted"></Card.Footer>
+    </Card>
+</>
+  )
+}
+
+export default Footer;
