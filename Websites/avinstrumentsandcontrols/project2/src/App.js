77fd27c43ca597152a6e143ebb0642e9eea9f531
codechini <arangathvenuchinmay@gmail.com>
2022-07-29 09:28:17 +0530

    tt

--- Websites/avinstrumentsandcontrols/project2/src/App.js
+++ Websites/avinstrumentsandcontrols/project2/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from '../src/components/Navigationbar';
+// import Main from './Pages/Home';
+import LearnMore from './Pages/LearnMore';
+import Products from './Pages/Products';
+import Footer from '../src/components/Footer';
+import Home from './Pages/Home';
+import Reg from './Pages/Registration';
+import login from './Pages/Login';
+
+import {
+  Card,
+  Col,
+  Row
+} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  //Link
+} from 'react-router-dom';
+import Login from './Pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    
+    <Router>
+      <Nav icon='AVInstrumentsandControls' l1='Home' l2='Products' l3='Learn More' quantity='(2)'/> 
+      {/* <Home title='AVIC'/> */}
+      {/* <Products/> */}
+      <Routes>
+        <Route path='/Home' element={<Home />} />
+        <Route path='/Products' element={<Products />}/>
+        <Route path='/LearnMore' element={<LearnMore />}/>
+        <Route path ='/RegPage' element={<Reg />}/>
+        <Route path ='/LoginPage' element={<Login />}/>
+      </Routes>
+      
+      <Footer l1='Contact Us' />
+
+    </Router>
+    </>
   );
 }
 
---
+++ Websites/avinstrumentsandcontrols/project2/src/components/Navigationbar.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import App from '../App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  // NavDropdown,
+  Navbar,
+  Nav,
+  // Form,
+  Container,
+  //Button
+}from 'react-bootstrap';
+
+import {BrowserRouter as Router,
+  Routes,
+  Route,
+  Link} from 'react-router-dom';
+
+import {FaCartPlus} from 'react-icons/fa';
+import { CgLogIn } from 'react-icons/cg';
+import { AiOutlineUserAdd } from 'react-icons/ai';
+
+
+const Navigationbar = (props) => {
+  return (
+    <>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand className='fs-4' ><Link className='brand' to="/Home">{props.icon}</Link></Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className=" mx-auto my-2 lg-1"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link ><Link className='links' to="/Home">{props.l1}</Link></Nav.Link>
+            <Nav.Link ><Link className='links' to="/Products">{props.l2}</Link></Nav.Link>
+            <Nav.Link ><Link className='links' to="/LearnMore">{props.l3}</Link></Nav.Link>
+          </Nav>
+          <div className="buttons d-flex justify-content-center">
+            <Link to='/RegPage' className="btn btn-outline text-white">
+              <AiOutlineUserAdd size='24px'/>&nbsp; {props.register}
+              <div className='carttxt'>Register</div>
+            </Link>
+            <Link to='/LoginPage' className="btn btn-outline text-white">
+              <CgLogIn size='24px'/>&nbsp; {props.login}
+              <div className='carttxt'>Login</div>
+            </Link>
+          
+            <a href="" className="btn btn-outline text-white">
+              <FaCartPlus size='24px'/>&nbsp; {props.quantity}
+              <div className='carttxt'>Add to Cart</div>
+            </a>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  </>
+  )
+}
+
+export default Navigationbar;
