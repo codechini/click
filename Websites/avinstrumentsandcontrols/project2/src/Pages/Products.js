b7ee3a22cc96f20b40af32aafaffd93db7f2c1b4
codechini <arangathvenuchinmay@gmail.com>
2022-07-29 19:42:14 +0530

    tt

--- Websites/avinstrumentsandcontrols/project2/src/Pages/Products.js
+++ Websites/avinstrumentsandcontrols/project2/src/Pages/Products.js
@@ -47,7 +47,7 @@ const ShowProducts=()=>{
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Button variant="primary" size='lg'>Go somewhere</Button>
+        <Button variant="primary" size='lg'>Buy</Button>
       </Card.Body>
     </Card>
     </div>
--- Websites/avinstrumentsandcontrols/project2/src/Pages/Registration.js
+++ Websites/avinstrumentsandcontrols/project2/src/Pages/Registration.js
@@ -14,10 +14,35 @@ import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Registration = () => {
+
   return (
     <>
       <div className="frm justify-content-center align-items-center">
+
       <Form className='justify-items-center'>
+      <Row>
+      <Col sm={6} >
+      <Form.Group className="mb-2" controlId="formBasicText">
+        <Form.Label id='Text' value='text' >Company Name</Form.Label>
+        <Form.Control size='sm' className='inpt' type="text" placeholder="Enter Name" />
+        <Form.Text className="text-muted">
+        </Form.Text>
+      </Form.Group>
+      <Form.Group className="mb-2" controlId="formBasicEmail">
+        <Form.Label >Email Address</Form.Label>
+        <Form.Control size='sm' className='inpt' type="email" placeholder="Enter Email" />
+        <Form.Text className="text-muted">
+        </Form.Text>
+      </Form.Group>
+      <Form.Group className="mb-2" controlId="formBasicPassword">
+        <Form.Label >Password</Form.Label>
+        <Form.Control size='sm' className='inpt' type="text" placeholder="Enter Password" />
+        <Form.Text className="text-muted">
+        </Form.Text>
+      </Form.Group>
+      </Col>
+      
+      <Col sm={6}> 
       <Form.Group className="mb-2" controlId="formBasicEmail">
         <Form.Label >Company Name</Form.Label>
         <Form.Control size='sm' className='inpt' type="text" placeholder="Enter Name" />
@@ -36,15 +61,25 @@ const Registration = () => {
         <Form.Text className="text-muted">
         </Form.Text>
       </Form.Group>
+      </Col>
+      
+      </Row>
 
-      <Button className='btnReg align-items-center' variant="primary" type="submit">
-          Register Business
-      </Button>
+      <Container>
+        <Row>
+          <Col>
+          <Button className='btnReg' variant="primary" type="submit">
+            Register Business
+          </Button>
+          </Col>
+        </Row>
+      </Container>
+      
       <Container>
         <Row>
           <Col>
           <div className="tosignup py-2">
-            Already have an Accout<Link to="/LoginPage">Login</Link>
+            Already have an Accout <Link to="/LoginPage">Login</Link>
           </div>
           </Col>
         </Row>
@@ -53,7 +88,7 @@ const Registration = () => {
       </Form>
     </div>
     </>
-  )
+  );
 }
 
 export default Registration;
